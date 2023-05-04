d8c2d3ada00b9fd1755a7e7f96e7f9e3189c02b7
samswag01 <stnlrssl@gmail.com>
2023-05-04 22:06:03 +0100

    Completed the project

---
+++ README.md
@@ -0,0 +1,12 @@
+# SM CLONE
+- This is a clone of my favorite online football game.
+[Striker Manager](https://en.strikermanager.com)
+
+## Live Demonstration of the Clone
+[Live App](https://samswag01.github.io/SMClone)
+
+### Features
+- Added a little dummy feature of clicking Home to increase message number and Competition to decrease it.
+- Note that this does not happen in the original app, clicking home take you to the homepage, the page i cloned, and competition takes you to your league table.
+
+## if you see something that can be improved or bug in it, don't forget to reach out. Thanks.
---
+++ script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+let msgSpace = document.querySelector('.msg-no'),
+    timeSpace = document.querySelector('.cur-time'),
+    totalMsg = 0;
+
+function updateMsgColor() {
+    if (totalMsg > 0) {
+        msgSpace.style.color = 'red';
+    } else {
+        msgSpace.style.color = '#FFFFFF';
+    }
+}
+
+const updateTime = () => {
+    let date = new Date();
+    let hour = formatTime(date.getHours());
+    let minute = formatTime(date.getMinutes());
+    let curTime = `${hour}:${minute}`;
+    timeSpace.textContent = curTime;
+}
+
+function increaseMsg() {
+    totalMsg++;
+    msgSpace.textContent = totalMsg;
+    updateMsgColor();
+}
+
+function decreaseMsg() {
+    if (totalMsg>0) {
+        totalMsg--;
+        msgSpace.textContent = totalMsg;
+    }
+    updateMsgColor();
+}
+
+setInterval(updateTime, 10000);
+
